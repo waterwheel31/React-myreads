bdb5acdc6c28ba8b1c2429cd193418555cac1dd6
waterwheel31 <waterwheel31@gmail.com>
2020-04-15 21:13:32 +0400

    launched

---
+++ myreads/src/ListBooks.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom' 
+import PropTypes from 'prop-types' 
+
+
+class ListBooks extends Component{
+    
+    render(){
+        true 
+    }
+
+}
+
+export default ListBooks
